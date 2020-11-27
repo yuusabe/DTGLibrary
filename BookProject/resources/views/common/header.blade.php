73b328415f0362a1b6b1b4c310a48037372f04c2
真島　皓太郎 <kmashima@pasona.tech>
2020-11-27 15:39:00 +0900

    修正

--- BookProject/resources/views/common/header.blade.php
+++ BookProject/resources/views/common/header.blade.php
@@ -57,7 +57,18 @@
                     <?php
                     if ($mflag == "1")
                     {
-                        echo '@extends("admin_header")';
+                        echo '<button type="button" id="button" class="btn btn-outline-danger" onclick="location.href=\'https://www-cf.dtg-booklibrary.tk/RPA\'">
+                                    貸出情報
+                                </button>
+                                <button type="button" id="button" class="btn btn-outline-danger" onclick="location.href=\'https://www-cf.dtg-booklibrary.tk/book_add\'">
+                                    書籍登録
+                                </button>
+                                <button type="button" id="button" class="btn btn-outline-danger" onclick="location.href=\'https://www-cf.dtg-booklibrary.tk/category\'">
+                                    カテゴリ管理
+                                </button>
+                                <button type="button" id="button" class="btn btn-outline-danger" onclick="location.href=\'https://www-cf.dtg-booklibrary.tk/account_management\'">
+                                    アカウント管理
+                                </button>';
                     }
                     ?>
                     <button type="button" id="button" class="btn btn-outline-secondary" onclick="location.href='https://www-cf.dtg-booklibrary.tk/list_of_books'">
