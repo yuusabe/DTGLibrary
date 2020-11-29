37298e64f93c7ab2be34a1e5b674994a2be57944
yuusabe <yu21093261@gmail.com>
2020-11-30 02:02:32 +0900

    カテゴリ画面ビュー調整

--- BookProject/resources/views/category.blade.php
+++ BookProject/resources/views/category.blade.php
@@ -10,29 +10,41 @@
         <p>カテゴリを追加してください。</p>
     </div>
     <div id="category_a">
-        <div id="category">
-            <input type="text" id="textbox" name="category" class="form-control" placeholder="入力して下さい">
-        </div>
+        <form action="{{ route('category.post') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div id="category">
+                <input type="text" id="textbox" name="category" class="form-control" placeholder="入力して下さい">
+            </div>
+            <div id="button_p">
+                <div id="button">
+                    <button type="submit" class="btn btn-outline-secondary" name = "add">
+                        登録
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
     <div id="text">
         <p>カテゴリを編集してください。</p>
     </div>
     <div id="category_p">
-        <div id="category_ch">
-            <input type="text" id="textbox_ch" name="category" class="form-control">
-        </div>
-        <div id="category_ch">
-            <div id="button_p">
-                <div id="button">
-                    <button type="buuton" class="btn btn-outline-secondary">
-                        編集
-                    </button>
-                </div>
-                <div id="button">
-                    <button type="button" class="btn btn-outline-secondary">
-                    削除
-                </button>
+        @foreach($category as $c)
+            <div id="category_ch">
+                <input type="text" id="textbox_ch" name="category" class="form-control" value = "{{c['category_name']}}">
             </div>
+            <div id="category_ch">
+                <div id="button_p">
+                    <div id="button">
+                        <button type="submit" class="btn btn-outline-secondary" name = "change">
+                            編集
+                        </button>
+                    </div>
+                    <div id="button">
+                        <button type="submit" class="btn btn-outline-secondary" name = "delete">
+                            削除
+                        </button>
+                </div>
+        @endforeach
         </div>
     </div>
 </main>
