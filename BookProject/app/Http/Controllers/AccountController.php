ea8a04eff46d03adad0a588e8bdd5474dd3348b5
Shotaro Takeyama <shotaro@dotinstall.com>
2020-11-30 02:19:17 +0900

    log_input1デバッグ

--- BookProject/app/Http/Controllers/AccountController.php
+++ BookProject/app/Http/Controllers/AccountController.php
@@ -163,6 +163,7 @@ class AccountController extends Controller
     function send1(Request $request){
         //セッションから値を取り出す
         $input1 = $request->session()->get("account_input_change");
+        Log::debug($input1);
         $number = $input1[0];
         Account::where(‘a_logic_flag’, TRUE)
         ->where('account_number', $number)->update([
